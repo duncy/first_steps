85b22af4c24064909635d4e11a746bbcf1653b80
duncy <fryerduncan@gmail.com>
2024-10-31 16:59:04 +1300

    REI compatibility, mod compat requirements, gradle & fabric loom update

---
+++ src/main/java/nz/duncy/first_steps/compat/FirstStepsREIClientPlugin.java
@@ -0,0 +1,33 @@
+package nz.duncy.first_steps.compat;
+
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import nz.duncy.first_steps.block.ModBlocks;
+import nz.duncy.first_steps.recipe.KilningRecipe;
+import nz.duncy.first_steps.screen.KilnScreen;
+
+
+public class FirstStepsREIClientPlugin implements REIClientPlugin {
+    @Override
+    public void registerCategories(CategoryRegistry registry) {
+        registry.add(new KilningCategory());
+
+        registry.addWorkstations(KilningCategory.FUEL_WASTE, EntryStacks.of(ModBlocks.KILN));
+    }
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        registry.registerRecipeFiller(KilningRecipe.class, KilningRecipe.Type.INSTANCE,
+                KilningDisplay::new);
+    }
+
+    @Override
+    public void registerScreens(ScreenRegistry registry) {
+        registry.registerClickArea(screen -> new Rectangle(75, 30, 20, 30), KilnScreen.class,
+                KilningCategory.FUEL_WASTE);
+    }
+}
